0bbf6544b8fa92ea935184120502a9db486cc8fc
Scott Crowder <scrowder@gmail.com>
2025-02-02 11:14:45 -0600

    Initial Commit

---
+++ TabViewExample/TabViewExampleApp.swift
@@ -0,0 +1,20 @@
+//
+// File: TabViewExampleApp.swift
+// Project: TabViewExample
+// 
+// Created by SCOTT CROWDER on 2/2/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct TabViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
